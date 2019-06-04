39c1f9d4c2a3e2d10d6429abbbd07db062c1ddac
Michal Dobiezynski <Michal.Dobiezynski@woodmac.com>
2019-06-04 09:05:28 +0100

    added a couple more assert equal tests

--- src/test/java/com/wiredbraincoffee/reward/RewardByDiscountServiceTest.java
+++ src/test/java/com/wiredbraincoffee/reward/RewardByDiscountServiceTest.java
@@ -1,7 +1,11 @@
 package com.wiredbraincoffee.reward;
 
 
+import com.wiredbraincoffee.product.Product;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,5 +20,31 @@ public class RewardByDiscountServiceTest {
 
         assertEquals(100, reward.getNeededPoints());
 
+    }
+
+
+    @Test
+    void setPercentageForPoints() {
+        RewardByDiscountService reward = new RewardByDiscountService();
+
+        reward.setPercentage(0.1);
+
+        assertEquals(0.1, reward.getPercentage());
+    }
+
+    @Test
+    void zeroCustomerPointS() {
+        RewardByDiscountService reward = new RewardByDiscountService();
+
+        reward.setPercentage(0.1);
+        reward.setNeededPoints(100);
+
+        Product smallDecaf = new Product(1, "Small Decaf", 1.99);
+        List<Product> order = Collections.singletonList(smallDecaf);
+
+        RewardInformation info = reward.applyReward(order, 0);
+
+        assertEquals(0, info.getDiscount());
+        assertEquals(0, info.getPointsRedeemed());
     }
 }
