d032a7b90fec643c35b926d961428a2adc8b18ba
Michal Dobiezynski <Michal.Dobiezynski@woodmac.com>
2019-06-04 08:58:27 +0100

    first assert equals test for a setter method of a class

--- src/test/java/com/wiredbraincoffee/reward/RewardByDiscountServiceTest.java
+++ src/test/java/com/wiredbraincoffee/reward/RewardByDiscountServiceTest.java
@@ -1,4 +1,20 @@
 package com.wiredbraincoffee.reward;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RewardByDiscountServiceTest {
+
+    @Test
+    void setNeededPoints() {
+
+        RewardByDiscountService reward = new RewardByDiscountService();
+
+        reward.setNeededPoints(100);
+
+        assertEquals(100, reward.getNeededPoints());
+
+    }
 }
