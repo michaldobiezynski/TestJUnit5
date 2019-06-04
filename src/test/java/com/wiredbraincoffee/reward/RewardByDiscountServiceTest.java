d51105135efbb93bc6213be593f4e3915340b2f8
Michal Dobiezynski <Michal.Dobiezynski@woodmac.com>
2019-06-04 09:57:55 +0100

    created nested test classes

--- src/test/java/com/wiredbraincoffee/reward/RewardByDiscountServiceTest.java
+++ src/test/java/com/wiredbraincoffee/reward/RewardByDiscountServiceTest.java
@@ -17,25 +17,7 @@ public class RewardByDiscountServiceTest {
     private List<Product> smallOrder;
     private List<Product> bigOrder = null;
 
-
-    @BeforeEach
-    void setUp() {
-
-        reward = new RewardByDiscountService();
-        reward.setPercentage(0.1);
-        reward.setNeededPoints(100);
-
-        Product smallDecaf = new Product(1, "Small Decaf", 1.99);
-        smallOrder = Collections.singletonList(smallDecaf);
-
-        Product bigDecaf = new Product(2, "Big Decaf", 2.49);
-        Product bigLatte = new Product(3, "Big Latte", 2.99);
-        Product bigTea = new Product(4, "Big Tea", 2.99);
-        Product espresso = new Product(5, "Espresso", 2.99);
-        bigOrder = Arrays.asList(bigTea, bigDecaf, bigLatte, espresso);
-
-    }
-
+    
     @Test
     void setNeededPoints() {
         System.out.println("Test setNeededPoints");
@@ -52,21 +34,56 @@ public class RewardByDiscountServiceTest {
 
     }
 
-    @Test
-    void zeroCustomerPointS() {
+    @Nested
+    class SmallOrder {
+        private List<Product> smallOrder = null;
 
-        RewardInformation info = reward.applyReward(smallOrder, 0);
+        @BeforeEach
+        void setUp() {
+            Product smallDecaf = new Product(1, "Small Decaf", 1.99);
+            smallOrder = Collections.singletonList(smallDecaf);
+        }
 
-        assertEquals(0, info.getDiscount());
-        assertEquals(0, info.getPointsRedeemed());
+
+        @Test
+        void zeroCustomerPointS() {
+
+            RewardInformation info = reward.applyReward(smallOrder, 0);
+
+            assertEquals(0, info.getDiscount());
+            assertEquals(0, info.getPointsRedeemed());
+        }
+
     }
 
-    @Test
-    void enoughtCustomerPointsForDiscountInBigOrder() {
 
-        RewardInformation info = reward.applyReward(bigOrder, 200);
+    @Nested
+    class BigOrder {
 
-        assertEquals(1.14, info.getDiscount(), 0.01);
-        assertEquals(100, info.getPointsRedeemed());
+        @BeforeEach
+        void setUp() {
+
+            Product bigDecaf = new Product(2, "Big Decaf", 2.49);
+            Product bigLatte = new Product(3, "Big Latte", 2.99);
+            Product bigTea = new Product(4, "Big Tea", 2.99);
+            Product espresso = new Product(5, "Espresso", 2.99);
+            bigOrder = Arrays.asList(bigTea, bigDecaf, bigLatte, espresso);
+
+        }
+
+        @Test
+        void enoughtCustomerPointsForDiscountInBigOrder() {
+
+            RewardInformation info = reward.applyReward(bigOrder, 200);
+
+            assertEquals(1.14, info.getDiscount(), 0.01);
+            assertEquals(100, info.getPointsRedeemed());
+        }
     }
+
+
 }
+
+
+
+
