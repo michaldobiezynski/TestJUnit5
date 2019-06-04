96bf2d6b5c187b684f633d3ee1212473722577d6
Michal Dobiezynski <Michal.Dobiezynski@woodmac.com>
2019-06-04 09:51:06 +0100

    mocked a big order and created a assert test about customer getting 100 points

--- src/test/java/com/wiredbraincoffee/reward/RewardByDiscountServiceTest.java
+++ src/test/java/com/wiredbraincoffee/reward/RewardByDiscountServiceTest.java
@@ -4,6 +4,7 @@ package com.wiredbraincoffee.reward;
 import com.wiredbraincoffee.product.Product;
 import org.junit.jupiter.api.*;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,6 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RewardByDiscountServiceTest {
 
     private RewardByDiscountService reward = null;
+    private List<Product> smallOrder;
+    private List<Product> bigOrder = null;
 
 
     @BeforeEach
@@ -22,7 +25,14 @@ public class RewardByDiscountServiceTest {
         reward.setPercentage(0.1);
         reward.setNeededPoints(100);
 
-        System.out.println("BeforeEach");
+        Product smallDecaf = new Product(1, "Small Decaf", 1.99);
+        smallOrder = Collections.singletonList(smallDecaf);
+
+        Product bigDecaf = new Product(2, "Big Decaf", 2.49);
+        Product bigLatte = new Product(3, "Big Latte", 2.99);
+        Product bigTea = new Product(4, "Big Tea", 2.99);
+        Product espresso = new Product(5, "Espresso", 2.99);
+        bigOrder = Arrays.asList(bigTea, bigDecaf, bigLatte, espresso);
 
     }
 
@@ -44,13 +54,19 @@ public class RewardByDiscountServiceTest {
 
     @Test
     void zeroCustomerPointS() {
-        
-        Product smallDecaf = new Product(1, "Small Decaf", 1.99);
-        List<Product> order = Collections.singletonList(smallDecaf);
 
-        RewardInformation info = reward.applyReward(order, 0);
+        RewardInformation info = reward.applyReward(smallOrder, 0);
 
         assertEquals(0, info.getDiscount());
         assertEquals(0, info.getPointsRedeemed());
+    }
+
+    @Test
+    void enoughtCustomerPointsForDiscountInBigOrder() {
+
+        RewardInformation info = reward.applyReward(bigOrder, 200);
+
+        assertEquals(1.14, info.getDiscount(), 0.01);
+        assertEquals(100, info.getPointsRedeemed());
     }
 }
