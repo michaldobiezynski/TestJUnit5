2a07ec724cdba73ff4cd4d4f8af10acb7fa65041
Michal Dobiezynski <Michal.Dobiezynski@woodmac.com>
2019-06-06 09:34:39 +0100

    dynamic container tests by containers

--- src/test/java/com/wiredbraincoffee/reward/RewardByGiftServiceDynamicTest.java
+++ src/test/java/com/wiredbraincoffee/reward/RewardByGiftServiceDynamicTest.java
@@ -1,9 +1,10 @@
 package com.wiredbraincoffee.reward;
 
+
 import com.wiredbraincoffee.product.Product;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
+import com.wiredbraincoffee.reward.RewardByGiftService;
+import com.wiredbraincoffee.reward.RewardInformation;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.function.ThrowingConsumer;
 
 import java.util.*;
@@ -11,13 +12,13 @@ import java.util.function.Function;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
 import static org.junit.jupiter.api.DynamicTest.stream;
 
 public class RewardByGiftServiceDynamicTest {
-
-    private RewardByGiftService reward;
+    private RewardByGiftService reward = null;
 
     @BeforeEach
     void setUp() {
@@ -27,17 +28,43 @@ public class RewardByGiftServiceDynamicTest {
     }
 
 //    @TestFactory
-//    Iterator<DynamicTest> dynamicTestCollection() {
+//    Collection<DynamicTest> dynamicTestsFromCollection() {
 //        return Arrays.asList(
 //                dynamicTest(
-//                        "1st dynamic test",() -> assertEquals(1,1)),
+//                        "1st dynamic test",
+//                        () -> assertEquals(11, 1)),
 //                dynamicTest(
-//                        "2nd dynamic test",() -> assertEquals(1,1))
-//                ).iterator();
+//                        "2nd dynamic test",
+//                        () -> assertEquals(1, 1))
+//        );
 //    }
 
-//        @TestFactory
-//        Stream<DynamicTest> giftProductNotInOrderRewardNotApplied() {
+//    @TestFactory
+//    Iterable<DynamicTest> dynamicTestsFromCollection() {
+//        return Arrays.asList(
+//                dynamicTest(
+//                        "1st dynamic test",
+//                        () -> assertEquals(1, 1)),
+//                dynamicTest(
+//                        "2nd dynamic test",
+//                        () -> assertEquals(1, 1))
+//        );
+//    }
+
+//    @TestFactory
+//    Iterator<DynamicTest> dynamicTestsFromCollection() {
+//        return Arrays.asList(
+//                dynamicTest(
+//                        "1st dynamic test",
+//                        () -> assertEquals(1, 1)),
+//                dynamicTest(
+//                        "2nd dynamic test",
+//                        () -> assertEquals(1, 1))
+//        ).iterator();
+//    }
+
+//    @TestFactory
+//    Stream<DynamicTest> giftProductNotInOrderRewardNotApplied() {
 //        return getStreamOfRandomNumbers()
 //                .limit(5)
 //                .mapToObj(randomId ->
@@ -77,21 +104,38 @@ public class RewardByGiftServiceDynamicTest {
         );
     }
 
+    @TestFactory
+    Stream<DynamicContainer> dynamicTestsWithContainers() {
+        return LongStream.range(1, 6)
+                .mapToObj(productId -> dynamicContainer(
+                        "Container for ID " + productId,
+                        Stream.of(
+                                dynamicTest("Valid Id", () -> assertTrue(productId > 0)),
+                                dynamicContainer("Test", Stream.of(
+                                        dynamicTest("Discount applied", () -> {
+                                            reward.setGiftProductId(productId);
+                                            RewardInformation info = reward.applyReward(
+                                                    getSampleOrder(), 200);
+
+                                            assertTrue(info.getDiscount() > 0);
+                                        })
+                                ))
+                        )
+                ));
+    }
+
     private LongStream getStreamOfRandomNumbers() {
         Random r = new Random();
         return r.longs(1000,2000);
-
     }
 
     private List<Product> getSampleOrder() {
-
         Product smallDecaf = new Product(1, "Small Decaf", 1.99);
-        Product bigDecaf = new Product(2, "bigDecaf", 2.49);
-        Product bigLatte = new Product(1, "bigLatte", 2.99);
-        Product bigTea = new Product(1, "bigTea", 2.99);
-        Product Espresso = new Product(1, "Espresso", 2.99);
-
-        return Arrays.asList(smallDecaf,bigDecaf,bigLatte,bigTea,Espresso);
-
+        Product bigDecaf = new Product(2, "Big Decaf", 2.49);
+        Product bigLatte = new Product(3, "Big Latte", 2.99);
+        Product bigTea = new Product(4, "Big Tea", 2.99);
+        Product espresso = new Product(5, "Espresso", 2.99);
+        return Arrays.asList(
+                smallDecaf, bigDecaf, bigLatte, bigTea, espresso);
     }
 }
